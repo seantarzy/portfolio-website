039c64db2085f84b74e969d42f5dbef7f2c3f9da
Sean Tarzy <seantarzy@gmail.com>
2020-12-29 16:30:42 -0500

    astronaut-projects

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Home from './Home'
 import Projects from './Projects'
 import Videos from './Videos'
 import About from './About'
+import mnemonicMaker from "./assets/mnemonic-maker.png";
+import seaMonsterRiddle from "./assets/sea-monster-riddle.png";
+import teacherToCoder from "./assets/teacher-to-coder.png";
+import genuineLeader from "./assets/genuine-leader.png";
+import Babe from './assets/babe-ruth.png'
 import {
   Switch,
   Route,
@@ -15,56 +20,80 @@ import {
   Redirect,
 } from "react-router-dom";
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "Mnemonic Maker",
+        imageSrc: mnemonicMaker,
+        link: "https://mnemonicmaker.netlify.app/",
+      },
+      {
+        id: 2,
+        name: "Sea Monster TedEd Riddle",
+        imageSrc: seaMonsterRiddle,
+        link: "https://github.com/seanytdawg/Sea-Monster-TED-Ed-Riddle",
+      },
+      {
+        id: 3,
+        name: "War Games",
+        imageSrc: Babe,
+        link: "https://github.com/seanytdawg/war_games",
+      },
+      {},
+      {},
+      {},
+      {}
+    ],
+    blogs: [
+      {
+        id: 1,
+        title: "School Teacher to coder",
+        imageSrc: teacherToCoder,
+        link:
+          "https://medium.com/swlh/how-being-a-school-teacher-set-me-up-to-be-a-coder-3d977cbc30b3",
+      },
+      {
+        id: 2,
+        title: "The Genuine Leader pt. 1",
+        imageSrc: genuineLeader,
+        link: "https://seantarzy.medium.com/the-genuine-leader-39ed769e5b7c",
+      },
+      {
+        id: 3,
+        title: "Sea Monster TedEd Riddle Blog",
+        imageSrc: seaMonsterRiddle,
+        link:
+          "https://seantarzy.medium.com/solving-the-sea-monster-ted-ed-riddle-using-vanilla-javascript-800fe02be6e0",
+      },
+    ],
+    videos: [],
+  };
 
-   renderBlogs = ()=>{
-     return <Blogs redirect={ () => this.props.history.push("/blogs")}/>
-   }
-   renderHome = ()=>{
-     return <Home redirect={ () => this.props.history.push("/home")}/>
-   }
-
-  render(){
-
-  return (
-    <div className="App">
-      <NavBar className="nav" />
-      <Router>
-          <Route
-            path="/blogs"
-            key={"blogs"}
-          >
-            <Blogs className= "blogs"/>
+  render() {
+    return (
+      <div className="App">
+        <NavBar className="nav" />
+        <Router>
+          <Route path="/blogs" key={"blogs"}>
+            <Blogs className="blogs" blogs={this.state.blogs} />
           </Route>
-          <Route
-            path="/projects"
-            key={"projects"}
-          >
-            <Projects className= "projects"/>
+          <Route path="/projects" key={"projects"}>
+            <Projects className="projects" projects={this.state.projects} />
           </Route>
-          <Route
-            path="/videos"
-            key={"videos"}
-          >
-            <Videos className= "videos"/>
+          <Route path="/videos" key={"videos"}>
+            <Videos className="videos" />
           </Route>
-          <Route
-            path="/about"
-            key={"about"}
-          >
-            <About className= "about"/>
+          <Route path="/about" key={"about"}>
+            <About className="about" />
           </Route>
-          <Route
-            exact
-            path="/"
-            key={"home"}
-            exact
-          >
-            <Home/>
+          <Route exact path="/" key={"home"} exact>
+            <Home />
           </Route>
-      </Router>
-    </div>
-  );
+        </Router>
+      </div>
+    );
   }
 }
 
--- src/Projects.js
+++ src/Projects.js
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
 import './App.css'
 import Spaceship from './assets/spaceship.png'
+import Astronaut from './assets/astronaut.png'
+import {Button} from 'react-bootstrap'
+import Modal from 'react-modal'
 class Projects extends Component {
-    render() {
-        return (
-            <div className = "projects"> 
-                <text>yo projects</text>
-                <div className="spaceship">
-                <img src = {Spaceship}/>
-                </div>
-            </div>
-        );
-    }
+  state = {
+    showModal: false,
+    featuredProject: null,
+  };
+  componentDidMount = () => {
+    this.props.projects.forEach((project) => {
+      let projectName = document.createElement("footer");
+      projectName.innerText = project.name;
+      let astroDiv = document.createElement("div");
+      let astroImg = document.createElement("img");
+      astroImg.src = Astronaut;
+      astroImg.height = "100";
+      astroImg.width = "100";
+      astroDiv.append(astroImg);
+      astroDiv.append(projectName);
+      astroDiv.className = "astronaut";
+      astroDiv.onclick = (e)=>{this.openModalAndSetFeatured(e, project)}
+      document.getElementsByClassName("project-astronauts")[0].append(astroDiv);
+    });
+  };
+
+  handleClose = () => {
+    this.setState({ showModal: false });
+  };
+
+  openModalAndSetFeatured = (e, project) => {
+      e.preventDefault()
+    this.setState({ showModal: true, featuredProject: project });
+    console.log("state: ", this.state)
+  };
+  render() {
+    return (
+      <div className="projects">
+        <text className="animated-text"> Projects</text>
+        <div className="spaceship">
+          <img src={Spaceship} />
+        </div>
+        <div className="project-astronauts">
+          {this.state.featuredProject ? (
+            <Modal
+              isOpen={this.state.showModal}
+              onRequestClose={this.handleClose}
+              animation={false}
+              className="modal"
+            >
+              <div>
+                <h1>{this.state.featuredProject.name}</h1>
+                {this.state.featuredProject.description}
+                <footer>
+                  <img
+                    src={this.state.featuredProject.imageSrc}
+                    className="modal-image"
+                  />
+                  Check it out:
+                  <a href={this.state.featuredProject.link}
+                    target = "_blank"
+                  >
+                    {this.state.featuredProject.name}
+                  </a>
+                  <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                  </Button>
+                </footer>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Projects;
